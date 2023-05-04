38c8f2b708ce8c3f11b6c68a405bee729eaaf0f9
michaelfeeney <michaelwcfeeney@gmail.com>
2023-05-04 14:18:35 -0400

    OH my, I'm so glad this class is mid-afternoon nap time!

--- OpenGLIsDahBest/OpenGLIsDahBest/someFunctionsImGonnaUse.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Pro tips:
-// * No code here (unless it's a template)
-//   (It's almost NEVER needing to be a template)
-// * any variable needs "extern" 
-int AddNumbers(int x, int y);
-
-extern int x;
